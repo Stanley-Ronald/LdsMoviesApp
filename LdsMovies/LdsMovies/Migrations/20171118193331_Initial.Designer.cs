970e2cbd85f3bcc684c51aa9e1c6bc4e5a212cb9
Ronald Stanley <stanleyronaldc@gmail.com>
2017-11-18 17:54:35 -0700

    Add files via upload
    Initial commit

---
+++ LdsMovies/LdsMovies/Migrations/20171118193331_Initial.Designer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using LdsMovies.Models;
+
+namespace LdsMovies.Migrations
+{
+    [DbContext(typeof(LdsMoviesContext))]
+    [Migration("20171118193331_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("LdsMovies.Models.Movie", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Genre");
+
+                    b.Property<decimal>("Price");
+
+                    b.Property<DateTime>("ReleaseDate");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Movie");
+                });
+        }
+    }
+}
---
+++ LdsMovies/LdsMovies/Models/MovieGenreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using LdsMovies.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace LdsMovies.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> movies;
+        public SelectList genres;
+        public string movieGenre { get; set; }
+    }
+}
